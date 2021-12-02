b89475d293543f8e95554abad5962e52224ebfac
Kamronbek <38859411+kamronbek29@users.noreply.github.com>
2021-12-01 21:14:05 -0800

    catch all Exception in send message

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,3 +1,5 @@
+import json
+
 from data.config import ADMINS_ID
 from data.messages import msg_dict
 
@@ -7,11 +9,14 @@ from keyboards.buttons import start_markup
 
 # Function to send waiting message
 async def send_message(chat_id, msg, args=None, markup=None, parse=None, reply=None):
-    msg_to_send = await user_msg(msg, args)
-    sent_message = await bot.send_message(chat_id, msg_to_send, reply_markup=markup, parse_mode=parse,
-                                          disable_web_page_preview=True, disable_notification=True,
-                                          reply_to_message_id=reply)
-    return sent_message.message_id
+    try:
+        msg_to_send = await user_msg(msg, args)
+        sent_message = await bot.send_message(chat_id, msg_to_send, reply_markup=markup, parse_mode=parse,
+                                              disable_web_page_preview=True, disable_notification=True,
+                                              reply_to_message_id=reply)
+        return sent_message.message_id
+    except Exception as err:
+        print('Error in send_message', err)
 
 
 async def send_voice(chat_id, file_to_send, caption, args=None, markup=None):
@@ -35,9 +40,14 @@ async def user_msg(message_str, args):
 
 # Save user id If it does not exists in DB
 async def check_user_info(chat_id):
-    user_lang_db = users_db.get(chat_id)
-    if user_lang_db is None:
-        users_db.set(chat_id, 'active')
+    user_info_db = users_db.get(chat_id)
+    if user_info_db is None:
+        user_info = {}
+        users_db.set(chat_id, json.dumps(user_info))
+    else:
+        user_info = json.loads(user_info_db)
+
+    return user_info
 
 
 # Send notification to admin that bot started working
