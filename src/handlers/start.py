0db28d39be0cf720d9ef41b2891e934789aaee02
shakhriyor <khudoyberdiev.shakhriyor@mail.com>
2022-10-18 18:05:21 +0500

    minor change

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -2,7 +2,13 @@ from time import sleep
 from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.dispatcher import FSMContext
 
-from keyboards.buttons import accents_markup, genders_markup, native_languages_markup, start_markup
+from keyboards.buttons import (
+    accents_markup,
+    genders_markup,
+    native_languages_markup,
+    start_markup,
+    sweatshirt_size_markup
+    )
 from utils.helpers import send_message, check_user_info
 from main import UserRegistration, dp
 from utils.uzbekvoice.helpers import check_if_correct_year, native_language, register_user
@@ -42,6 +48,19 @@ async def get_name(message: Message, state: FSMContext):
         await UserRegistration.next()
 
 
+@dp.message_handler(state=UserRegistration.year_of_birth)
+async def get_birth_year(message: Message, state: FSMContext):
+    async with state.proxy() as data:
+        if check_if_correct_year(message.text):
+            data["year_of_birth"] = message.text
+            await send_message(data["tg_id"], 'ask-native-language', markup=native_languages_markup)
+            await UserRegistration.next()
+        else:
+            await send_message(data["tg_id"], 'ask-birth-year-again')
+            return await UserRegistration.year_of_birth.set()
+        await UserRegistration.next()
+
+
 @dp.message_handler(state=UserRegistration.gender)
 async def get_gender(message: Message, state: FSMContext):
     async with state.proxy() as data:
@@ -54,26 +73,31 @@ async def get_gender(message: Message, state: FSMContext):
     await UserRegistration.next()
 
 
+@dp.message_handler(state=UserRegistration.phone_number)
+async def get_phone(message: Message, state: FSMContext):
+    async with state.proxy() as data:
+        data["phone_number"] = message.text
+
+    await send_message(data["tg_id"], 'ask-phone-number')
+    await UserRegistration.next()
+
+
+@dp.message_handler(state=UserRegistration.gender)
+async def get_sweatshirt_size(message: Message, state: FSMContext):
+    async with state.proxy() as data:
+        data["sweatshirt_size"] = message.text
+
+    await send_message(data["tg_id"], 'ask-sweatshirt-size', markup=sweatshirt_size_markup)
+    await UserRegistration.next()
+
+
 @dp.message_handler(state=UserRegistration.accent_region)
 async def get_accent_region(message: Message, state: FSMContext):
     async with state.proxy() as data:
         data["accent_region"] = message.text
 
-    await send_message(data["tg_id"], 'ask-birth-year', markup=ReplyKeyboardRemove())
+    await send_message(data["tg_id"], 'ask-accent-region', markup=ReplyKeyboardRemove())
     await UserRegistration.next()
-
-
-@dp.message_handler(state=UserRegistration.year_of_birth)
-async def get_birth_year(message: Message, state: FSMContext):
-    async with state.proxy() as data:
-        if check_if_correct_year(message.text):
-            data["year_of_birth"] = message.text
-            await send_message(data["tg_id"], 'ask-native-language', markup=native_languages_markup)
-            await UserRegistration.next()
-        else:
-            await send_message(data["tg_id"], 'ask-birth-year-again')
-            return await UserRegistration.year_of_birth.set()
-        await UserRegistration.next()
 
 
 @dp.message_handler(state=UserRegistration.finish)
