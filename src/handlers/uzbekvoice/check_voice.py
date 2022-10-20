24182604cad48b7fbf0861bf856c894c93b31958
shakhriyor <khudoyberdiev.shakhriyor@mail.com>
2022-10-20 18:31:27 +0500

    updated requirements.txt

--- src/handlers/uzbekvoice/check_voice.py
+++ src/handlers/uzbekvoice/check_voice.py
@@ -57,7 +57,7 @@ async def ask_action_handler(call: CallbackQuery, state: FSMContext):
     if call_data == 'report':
         await edit_reply_markup(chat_id, message_id, report_voice_markup)
         await AskUserAction.report_type.set()
-        return
+
     elif call_data == 'skip':
         await call.message.delete()
     else:
