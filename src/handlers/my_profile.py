75a21274f2945dbd40d74735527795b7d477e77b
ASKAR <askarjon.abdullayev@gmail.com>
2022-10-31 21:58:08 +0500

    my profile

--- src/handlers/my_profile.py
+++ src/handlers/my_profile.py
@@ -3,12 +3,11 @@ from main import dp, bot
 from aiogram.types import Message
 
 from data.messages import MY_PROFILE
-from utils.helpers import send_message
 
 
 @dp.message_handler(text=MY_PROFILE)
 @dp.message_handler(commands=['my_profile'])
 async def instructions(message: Message):
     user = get_user(message.chat.id)
-    my_profile = f"👤 Mening profilim:\n\nIsm: {user[4]}"
-    await bot.send_message(message.chat.id, my_profile)
+    my_profile = f"<b>👤 Mening profilim:</b>\n\nIsm: <code>{user[4]}</code>\nTelefon raqam: <code>{user[5]}</code>\nYosh oralig'i: <code>{user[9]}</code>\nJinsi: <code>{'Erkak' if user[7] == 'M' else 'Ayol'}</code>\nShevasi: <code>{user[8]}</code>\nOna-tili: <code>{user[10]}</code>"
+    await bot.send_message(message.chat.id, my_profile, parse_mode="HTML")
